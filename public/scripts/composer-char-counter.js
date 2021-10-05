799757411a1955e2d03778fd4c70963b81fc8bd6
Ujjawal Sidhpura <ujjawalsidhpura@gmail.com>
2021-10-05 19:03:20 +0000

    Styling: tweets.css file added and formated into the web page

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
   //On every Refresh
-  const maxLength = 20;
+  const maxLength = 140;
   $('#letter-count-display').text(maxLength);
 
   $('#tweet-text').on('input', (e) => {
@@ -8,13 +8,15 @@ $(document).ready(function () {
     const currentLength = target.value.length;
     const displayLength = maxLength - currentLength;
 
+    //Makes displayer RED
     if (displayLength < 0) {
       $('#letter-count-display').addClass('red');
     }
-
+    //Makes displayer default color IF character count is positive.
     if (displayLength >= 0) {
       $('#letter-count-display').removeClass('red');
     }
+
     $('#letter-count-display').text(displayLength);
 
   })
